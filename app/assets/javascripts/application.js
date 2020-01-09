afd6a81acb0cfb5d87964f98886c72f2d08b323b
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-09 15:42:37 +0000

    heorku mess

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,20 +6,7 @@
 // var textWrapper = document.querySelector('.ml6 .letters');
 // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml6 .letter',
-//     translateY: ["1.1em", 0],
-//     translateZ: 0,
-//     duration: 750,
-//     delay: (el, i) => 50 * i
-//   }).add({
-//     targets: '.ml6',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
+
 
   // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml2');
@@ -33,7 +20,7 @@ anime.timeline({loop: true})
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 950,
-    delay: (el, i) => 70*i
+    delay: 70
   }).add({
     targets: '.ml2',
     opacity: 0,
@@ -41,51 +28,7 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
-  // main heading
 
-  // all h2
-  // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml12');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml12 .letter',
-//     translateX: [40,0],
-//     translateZ: 0,
-//     opacity: [0,1],
-//     easing: "easeOutExpo",
-//     duration: 1200,
-//     delay: (el, i) => 500 + 30 * i
-//   }).add({
-//     targets: '.ml12 .letter',
-//     translateX: [0,-30],
-//     opacity: [1,0],
-//     easing: "easeInExpo",
-//     duration: 1100,
-//     delay: (el, i) => 100 + 30 * i
-//   });
-
-// });
-
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml16');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml16 .letter',
-//     translateY: [-100,0],
-//     easing: "easeOutExpo",
-//     duration: 1400,
-//     delay: (el, i) => 30 * i
-//   }).add({
-//     targets: '.ml16',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-// scroll
 
 
 
